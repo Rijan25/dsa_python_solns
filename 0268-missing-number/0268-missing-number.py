aa41742ae334dfa481051519cfbca0802601ba54
Rijan Pokhrel <117513224+Rijan25@users.noreply.github.com>
2025-03-08 19:57:36 +0545

    Time: 0 ms (100%), Space: 18.9 MB (18.18%) - LeetHub

--- 0268-missing-number/0268-missing-number.py
+++ 0268-missing-number/0268-missing-number.py
@@ -1,5 +1,14 @@
 class Solution:
     def missingNumber(self, nums: List[int]) -> int:
+
+        # Optimal Solutions using Brute force approach
+        n=len(nums)
+        sum=0
+        total_sum=(n*(n+1))//2
+        for num in nums:
+            sum+=num
+        return total_sum-sum
+        # Brute Force Approach
         for index in range(len(nums)+1):
             if index not in nums:
                 return index
