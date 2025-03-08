302b0b5f56936ea6a293f800b6663e3d240a959d
Rijan Pokhrel <117513224+Rijan25@users.noreply.github.com>
2025-03-08 19:03:15 +0545

    Time: 0 ms (100%), Space: 18.3 MB (73.6%) - LeetHub

--- 0035-search-insert-position/0035-search-insert-position.py
+++ 0035-search-insert-position/0035-search-insert-position.py
@@ -1,13 +1,24 @@
 class Solution:
     def searchInsert(self, nums: List[int], target: int) -> int:
-        for index,val in enumerate(nums):
-            if target==val:
-                return index
-            elif target not in nums:
-                if target<val:
-                    return index
-        return len(nums)        
+        # Optimized Solutions
+        left, right = 0, len(nums) - 1
 
-         
+        while left <= right:
+            mid = (right + left) // 2
 
-        
+            if target == nums[mid]:
+                return mid
+            elif target > nums[mid]:
+                left = mid + 1
+            else:
+                right = mid - 1
+        return left
+
+        # Brute Force Approach
+        # for index,val in enumerate(nums):
+        #     if target==val:
+        #         return index
+        #     elif target not in nums:
+        #         if target<val:
+        #             return index
+        # return len(nums)
