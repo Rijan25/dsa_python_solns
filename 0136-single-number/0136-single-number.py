782765ac643934d7338dafdd1649288cf7f65bac
Rijan Pokhrel <117513224+Rijan25@users.noreply.github.com>
2025-03-08 19:39:58 +0545

    Time: 0 ms (100%), Space: 19.6 MB (44.43%) - LeetHub

--- 0136-single-number/0136-single-number.py
+++ 0136-single-number/0136-single-number.py
@@ -1,5 +1,12 @@
 class Solution:
     def singleNumber(self, nums: List[int]) -> int:
+        # most optimal xor operations
+        result=0
+        for num in nums:
+            result^=num
+        return result
+
+        # Hashmap solutions
         count={}
         for num in nums:
             if num in count:
