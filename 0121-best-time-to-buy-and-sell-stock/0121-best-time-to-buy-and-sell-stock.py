55914f66d724b3adcc5c0f2f9b4a66258d429705
Rijan Pokhrel <117513224+Rijan25@users.noreply.github.com>
2025-03-08 16:38:16 +0545

    Time: 126 ms (31.16%), Space: 26.9 MB (77.63%) - LeetHub

---
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.py
@@ -0,0 +1,26 @@
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        buy=prices[0]
+        profit=0
+        for num in prices:
+            profit=max(profit,num-buy)
+            buy=min(buy,num)
+        return profit    
+
+       
+          
+
+        #Brute Force Approach 
+        # profit=[]
+        # for l in range(len(prices)-1):
+        #     for r in range(l,len(prices)):
+        #         profit.append(prices[r]-prices[l])
+        # return max(profit,default=0)        
+            
+        
+
+
+
+               
+
+        
