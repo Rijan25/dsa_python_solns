2c691af934532fcb765d2daa9364382e40115da7
Rijan Pokhrel <117513224+Rijan25@users.noreply.github.com>
2025-03-08 14:38:06 +0545

    Time: 31 ms (34.39%), Space: 18 MB (82.59%) - LeetHub

--- 2486-most-frequent-even-element/2486-most-frequent-even-element.py
+++ 2486-most-frequent-even-element/2486-most-frequent-even-element.py
@@ -1,5 +1,6 @@
 class Solution:
     def mostFrequentEven(self, nums: List[int]) -> int:
+
         count={}
         nums.sort()
         for num in nums:
@@ -7,8 +8,20 @@ class Solution:
                 count[num]+=1
             elif num not in count and num%2==0:
                 count[num]=1
-        if len(count)==0:
+        if(len(count)==0):
             return -1
-        return max(count,key=lambda key:count[key])            
+        return (max(count,key=lambda key:count[key]))            
+
+
+        # count={}
+        # nums.sort()
+        # for num in nums:
+        #     if num in count and num%2==0:
+        #         count[num]+=1
+        #     elif num not in count and num%2==0:
+        #         count[num]=1
+        # if len(count)==0:
+        #     return -1
+        # return max(count,key=lambda key:count[key])            
 
         
