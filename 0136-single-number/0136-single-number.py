9e732df78ee5b91926ef8f6c498a3016cd381d26
Rijan Pokhrel <117513224+Rijan25@users.noreply.github.com>
2025-03-13 16:45:05 +0545

    Time: 3 ms (65.61%), Space: 19.5 MB (77.52%) - LeetHub

--- 0136-single-number/0136-single-number.py
+++ 0136-single-number/0136-single-number.py
@@ -1,17 +1,22 @@
 class Solution:
     def singleNumber(self, nums: List[int]) -> int:
-        # most optimal xor operations
+
         result=0
         for num in nums:
             result^=num
-        return result
+        return result    
+        # most optimal xor operations
+        # result=0
+        # for num in nums:
+        #     result^=num
+        # return result
 
-        # Hashmap solutions
-        count={}
-        for num in nums:
-            if num in count:
-                count[num]+=1
-            else:
-                count[num]=1
-        return min(count,key=lambda key: count[key])            
+        # # Hashmap solutions
+        # count={}
+        # for num in nums:
+        #     if num in count:
+        #         count[num]+=1
+        #     else:
+        #         count[num]=1
+        # return min(count,key=lambda key: count[key])            
         
