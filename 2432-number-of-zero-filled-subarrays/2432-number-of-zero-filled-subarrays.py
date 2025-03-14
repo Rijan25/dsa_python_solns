41c9ab33b080fcd628c0b68d3a719e7bd0a8b746
Rijan Pokhrel <117513224+Rijan25@users.noreply.github.com>
2025-03-14 19:55:48 +0545

    Time: 20 ms (98.63%), Space: 28.4 MB (40.38%) - LeetHub

--- 2432-number-of-zero-filled-subarrays/2432-number-of-zero-filled-subarrays.py
+++ 2432-number-of-zero-filled-subarrays/2432-number-of-zero-filled-subarrays.py
@@ -1,16 +1,30 @@
 class Solution:
     def zeroFilledSubarray(self, nums: List[int]) -> int:
+
         result=0
         count=0
         for num in nums:
             if num==0:
                 count+=1
-                
             else:
-                result+=count*(count+1)//2 
+                result+=count*(count+1)//2
                 count=0
-        # For Accounting the contibution of the last zero        
-        result+=count*(count+1)//2         
+
+        result+=count*(count+1)//2
+        return result            
+
+
+        # result=0
+        # count=0
+        # for num in nums:
+        #     if num==0:
+        #         count+=1
+                
+        #     else:
+        #         result+=count*(count+1)//2 
+        #         count=0
+        # # For Accounting the contibution of the last zero        
+        # result+=count*(count+1)//2         
               
-        return result         
+        # return result         
         
