1f76189d6d3f6ffad0bf6e55789fd9c107bcfe5f
Rijan Pokhrel <117513224+Rijan25@users.noreply.github.com>
2025-03-14 19:23:12 +0545

    Time: 6 ms (48.31%), Space: 25.7 MB (46.23%) - LeetHub

--- 0189-rotate-array/0189-rotate-array.py
+++ 0189-rotate-array/0189-rotate-array.py
@@ -4,18 +4,46 @@ class Solution:
         Do not return anything, modify nums in-place instead.
         """
 
-        # Optimal Solutions using reversal
+        # Revision session 1
         n=len(nums)
         k%=n
-        def reverse_array(start,end):
+
+        def rotate_array(start,end):
             while start<end:
                 nums[start],nums[end]=nums[end],nums[start]
                 start+=1
                 end-=1
 
-        reverse_array(0,n-1)
-        reverse_array(0,k-1)
-        reverse_array(k,n-1)
+        rotate_array(0,n-1)
+        rotate_array(0,k-1)
+        rotate_array(k,n-1) 
+             
+
+        # Optimal Solutions using reversals practise
+        # n=len(nums)
+        # k%=n
+        # def reverse_array(start,end):
+        #     while start<end:
+        #         nums[start],nums[end]=nums[end],nums[start]
+        #         start+=1
+        #         end-=1
+
+        # reverse_array(0,n-1)
+        # reverse_array(0,k-1)
+        # reverse_array(k,n-1)
+
+        # # Optimal Solutions using reversal
+        # n=len(nums)
+        # k%=n
+        # def reverse_array(start,end):
+        #     while start<end:
+        #         nums[start],nums[end]=nums[end],nums[start]
+        #         start+=1
+        #         end-=1
+
+        # reverse_array(0,n-1)
+        # reverse_array(0,k-1)
+        # reverse_array(k,n-1)
                
 
 
