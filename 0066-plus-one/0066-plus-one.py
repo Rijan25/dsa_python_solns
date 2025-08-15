6e096d3ff297a6c0a2564a02293222d97569b96a
Rijan Pokhrel <117513224+Rijan25@users.noreply.github.com>
2025-08-15 13:28:33 +0545

    Time: 0 ms (100%), Space: 17.9 MB (30.21%) - LeetHub

--- 0066-plus-one/0066-plus-one.py
+++ 0066-plus-one/0066-plus-one.py
@@ -1,9 +1,8 @@
 class Solution:
     def plusOne(self, digits: List[int]) -> List[int]:
-        result=0
-        ret_digits=[]
-        for index,num in enumerate(digits):
-            result=result*10+num
-        final_list=[int(x) for x in str(result+1)]
-        return final_list
+        number=''
+        for item in digits:
+            number+=str(item)
+        number=int(number)+1
+        return [int(x) for x in str(number).strip()]    
         
