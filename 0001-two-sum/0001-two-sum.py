a0a4df4c1952d7be20a331f2c47539d92f567793
Rijan Pokhrel <117513224+Rijan25@users.noreply.github.com>
2025-09-27 11:39:55 +0545

    Time: 0 ms (100%), Space: 18.8 MB (50.89%) - LeetHub

--- 0001-two-sum/0001-two-sum.py
+++ 0001-two-sum/0001-two-sum.py
@@ -1,9 +1,22 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
+        # Brute Force Approach
+        # for index,num in enumerate(nums):
+        #     for i in range(index+1,len(nums)):
+        #         if (target-num)==nums[i]:
+        #             return [index,i]
+
+        
+        # HashTable Approach
+        hashtable={}
+        n=len(nums)
+
         for index,num in enumerate(nums):
-            for i in range(index+1,len(nums)):
-                if (target-num)==nums[i]:
-                    return [index,i]
+            complement=target-num
+            if complement in hashtable:
+                return [index,hashtable[complement]]
+            hashtable[num]=index    
+
         
             
         
