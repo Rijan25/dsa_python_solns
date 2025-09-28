0a4867c773abfb71804b7c0b2ab7a9e0fc38f9bd
Rijan Pokhrel <117513224+Rijan25@users.noreply.github.com>
2025-09-28 16:05:55 +0545

    Time: 8 ms (37.08%), Space: 19.6 MB (18.74%) - LeetHub

--- 0169-majority-element/0169-majority-element.py
+++ 0169-majority-element/0169-majority-element.py
@@ -1,23 +1,62 @@
 class Solution:
     def majorityElement(self, nums: List[int]) -> int:
-        count={}
-        majority=len(nums)//2
+
+        # Hashmap Method
+        from collections import Counter
+        counts=Counter(nums)
+        majority=len(nums)/2
         for num in nums:
-            if num in count:
-                count[num]+=1
-            else:
-                count[num]=1
-            if(count[num]>majority):
-                return num    
-               
-        # count={}
-        # majority=len(nums)//2
+            if counts[num]>majority:
+                return num
+
+        # # Boyer Moore Voting Algorithm
+
+        # count,result=0,0
         # for num in nums:
-        #     if num in count:
-        #         count[num]+=1
+        #     if count==0:
+        #         result=num
         #     else:
-        #         count[num]=1
-        #     if(count[num]>majority):
-        #         return num
+        #         count+=(1 if result==num else -1) 
+        # return result           
+
+
+        # # count={}
+        # # majority=len(nums)//2
+        # # for num in nums:
+        # #     if num in count:
+        # #         count[num]+=1
+        # #     else :
+        # #         count[num]=1
+        # #     if count[num]>majority:
+        # #         return num    
+      
+                        
+
+        # # Boyer Moore Voting Algorithms
+
+        # # count,result=0,0
+        # # for num in nums:
+        # #     if count==0:
+        # #         result=num
+        # #     else:
+        # #         count+=(1 if result==num else -1)  
+        # # return result        
+
+
+        
+        # # Hashmap
+        # # count={}
+        # # majority=len(nums)//2
+        # # for num in nums:
+        # #     if num in count:
+        # #         count[num]+=1
+        # #     else:
+        # #         count[num]=1
+        # #     if(count[num]>majority):
+        # #         return num    
+                
+
+
+        
 
         
