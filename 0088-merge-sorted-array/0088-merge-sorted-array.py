dbca64a00c5f02dade1c6b12ffbdcfce0f14420c
Rijan Pokhrel <117513224+Rijan25@users.noreply.github.com>
2025-09-28 09:00:30 +0545

    Time: 0 ms (100%), Space: 17.7 MB (93.59%) - LeetHub

--- 0088-merge-sorted-array/0088-merge-sorted-array.py
+++ 0088-merge-sorted-array/0088-merge-sorted-array.py
@@ -3,10 +3,34 @@ class Solution:
         """
         Do not return anything, modify nums1 in-place instead.
         """
+        # Two Pointers Approach
+        i,j,k=m-1,n-1,m+n-1
+        while i>=0 and j>=0:
+            if nums1[i]>nums2[j]:
+                nums1[k]=nums1[i]
+                i-=1
+            else:
+                nums1[k]=nums2[j]
+                j-=1
+            k-=1
+        
+        # for the remaining elements of the small nums2
+        while j>=0:
+            nums1[k]=nums2[j]
+            j-=1
+            k-=1
 
-        for j in range(n):
-            nums1[m+j]=nums2[j]
-        nums1.sort()    
+
+
+
+
+
+
+        
+        # Simple Sorted
+        # for j in range(n):
+        #     nums1[m+j]=nums2[j]
+        # nums1.sort()    
          
 
         
